13d21afe79fb37b139d67c50b24b6243ecbc7661
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2018-11-27 07:46:05 +0200

    refactor exercise using symbols instead of strings for hash key

--- introduction_to_programming/2_the_basics/ex3_movie_titles.rb
+++ introduction_to_programming/2_the_basics/ex3_movie_titles.rb
@@ -1,5 +1,5 @@
-films = {"Star Wars" => 1971,
-         "Jaws" => 1930,
-         "Helm" => 1945,}
+films = {:"Star Wars" => 1971,
+         :"Jaws" => 1930,
+         :"Helm" => 1945,}
 
 films.each_value {|year| puts year}
