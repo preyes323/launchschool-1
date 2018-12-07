12ae3d7c1cafaacb442a60693539e592f7158e63
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2018-12-07 09:04:46 +0200

    refactor based on answers to exercise 15

--- introduction_to_programming/10_exercises/ex15.rb
+++ introduction_to_programming/10_exercises/ex15.rb
@@ -2,5 +2,4 @@ arr = ['snow', 'winter', 'ice', 'slippery', 'salted roads', 'white trees']
 arr.delete_if {|word| word.start_with?("s")}
 
 arr = ['snow', 'winter', 'ice', 'slippery', 'salted roads', 'white trees']
-arr.delete_if {|word| word.start_with?("s") || word.start_with?("w")}
-
+arr.delete_if {|word| word.start_with?("s", "w")}
