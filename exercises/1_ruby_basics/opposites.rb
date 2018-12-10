a410754d4d1ffa8eecafbbef054dafc18926afb9
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2018-12-10 09:19:09 +0200

    solve exercise and refactor another

--- exercises/1_ruby_basics/opposites.rb
+++ exercises/1_ruby_basics/opposites.rb
@@ -6,15 +6,23 @@ int1 = nil
 int2 = nil
 
 loop do
-  puts ">> Please enter a positive or negative integer:"
-  int1 = gets.chomp.to_i
-  puts ">> Please enter a positive or negative integer:"
-  int2 = gets.chomp.to_i
-  if int1 > 0 && int2 < 0
-    puts "#{int1} + #{int2} = #{int1+int2}"
+  loop do
+    puts ">> Please enter a positive or negative integer:"
+    int1 = gets.chomp
+    break if valid_number?(int1)
+    puts "Invalid input. Only non-zero integers are allowed."
+  end
+  loop do
+    puts ">> Please enter a positive or negative integer:"
+    int2 = gets.chomp
+    break if valid_number?(int2)
+    puts "Invalid input. Only non-zero integers are allowed."
+  end
+  if int1.to_i > 0 && int2.to_i < 0
+    puts "#{int1} + #{int2} = #{int1.to_i+int2.to_i}"
     break
-  elsif int1 < 0 && int2 > 0
-    puts "#{int1} + #{int2} = #{int1+int2}"
+  elsif int1.to_i < 0 && int2.to_i > 0
+    puts "#{int1} + #{int2} = #{int1.to_i+int2.to_i}"
     break
   else
     puts "Sorry. One integer must be positive, one must be negative."
