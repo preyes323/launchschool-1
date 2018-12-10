4e3b80a1392cd406ae978f9bba48de99a20b7455
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2018-12-10 08:52:31 +0200

    refactor exercise solution

--- exercises/1_ruby_basics/division.rb
+++ exercises/1_ruby_basics/division.rb
@@ -8,9 +8,19 @@ denominator = nil
 loop do
   puts ">> Please enter the numerator:"
   numerator = gets.chomp
-  puts ">> Please enter the denominator:"
-  denominator = gets.chomp
-  break if valid_number?(numerator) && valid_number?(denominator)
+  break if valid_number?(numerator)
   puts ">> Invalid input. Only integers are allowed."
 end
+
+loop do
+  puts ">> Please enter the denominator:"
+  denominator = gets.chomp
+  if denominator.to_i == 0
+    puts ">> Invalid input. A denominator of 0 is not allowed."
+    next
+  elsif valid_number?(denominator)
+    break
+  end
+end
+    
 puts "#{numerator} / #{denominator} is #{numerator.to_i / denominator.to_i}"
