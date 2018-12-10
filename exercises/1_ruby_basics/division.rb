47725aa30f59d73f6df662ffc01ca943a4a75e3a
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2018-12-10 08:44:26 +0200

    solve basic problem

---
+++ exercises/1_ruby_basics/division.rb
@@ -0,0 +1,16 @@
+def valid_number?(number_string)
+  number_string.to_i.to_s == number_string
+end
+
+numerator = nil
+denominator = nil
+
+loop do
+  puts ">> Please enter the numerator:"
+  numerator = gets.chomp
+  puts ">> Please enter the denominator:"
+  denominator = gets.chomp
+  break if valid_number?(numerator) && valid_number?(denominator)
+  puts ">> Invalid input. Only integers are allowed."
+end
+puts "#{numerator} / #{denominator} is #{numerator.to_i / denominator.to_i}"
