9f9bd3023dc83c02e6e3e5aafdd217bf9856e167
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2018-12-11 08:29:20 +0200

    solve exercise (brute force)

---
+++ exercises/1_ruby_basics/are_you_there.rb
@@ -0,0 +1,11 @@
+colors = 'blue pink yellow orange'
+
+case
+when colors.include?("yellow") then puts "true"
+else puts "false"
+end
+
+case
+when colors.include?("purple") then puts "true"
+else puts "false"
+end
