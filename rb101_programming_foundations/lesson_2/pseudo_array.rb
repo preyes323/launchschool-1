d8b3753d42875d0e95657df654c82f030204f9b5
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2018-12-13 07:42:44 +0200

    solve exercise 3 in pseudocode

---
+++ rb101_programming_foundations/lesson_2/pseudo_array.rb
@@ -0,0 +1,12 @@
+START
+
+GET array of integers [1,2,3 etc]
+SET variable to be length of int-array
+SET result-array to nil
+
+LOOP OVER THE ARRAY
+when length-of-array is even
+  push the item in the array to the final result-array
+end
+
+END
