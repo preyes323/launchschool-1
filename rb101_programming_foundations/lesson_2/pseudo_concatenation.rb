a9cf75ddbbfdbeff14d8736a70ccee8f7d8c87af
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2018-12-13 07:40:09 +0200

    solve exercise in pseudocode

---
+++ rb101_programming_foundations/lesson_2/pseudo_concatenation.rb
@@ -0,0 +1,14 @@
+START
+
+# takes an array of strings
+# returns a string with all those string concatenated together
+
+GET array of strings
+SET empty result (string) variable
+
+LOOP
+for every string, add it to the string variable
+
+PRINT the final product (i.e. the concatenated string variable)
+
+END
