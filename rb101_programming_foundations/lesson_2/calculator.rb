1392cec834ec7e534a61dfaacda64c89de456fae
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2018-12-13 07:25:06 +0200

    complete calculator exercise as per the video

--- rb101_programming_foundations/lesson_2/calculator.rb
+++ rb101_programming_foundations/lesson_2/calculator.rb
@@ -3,3 +3,26 @@
 # perform the operation on the two numbers
 # output the result
 
+Kernel.puts("Welcome to the Calculator!")
+
+Kernel.puts("What's the first number?")
+number1 = Kernel.gets().chomp()
+
+Kernel.puts("What's the second number?")
+number2 = Kernel.gets().chomp()
+
+Kernel.puts("What operation woudl you like to perform? 1) add 2) subtract 3) multiply 4) divide")
+operator = Kernel.gets().chomp()
+
+if operator == '1'
+  result = number1.to_i() + number2.to_i()
+elsif operator == '2'
+  result = number1.to_i() - number2.to_i()
+elsif operator == '3'
+  result = number1.to_i() * number2.to_i()
+elsif operator == '4'
+  result = number1.to_f() / number2.to_f()
+end
+
+Kernel.puts("The result is #{result}")
+
