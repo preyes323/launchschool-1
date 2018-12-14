3de24c65acbbaa0157ef915c4193d7fd51affd71
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2018-12-14 08:44:22 +0200

    refactored and added in course-approved solution

--- exercises/2_small_problems/easy1/odd.rb
+++ exercises/2_small_problems/easy1/odd.rb
@@ -17,6 +17,8 @@ puts is_odd?(7)    # => true
 
 =end
 
+# first solution to exercise with modulo
+
 def is_odd?(int)
   if int % 2 == 0 then false
   else true
@@ -29,7 +31,12 @@ puts is_odd?(-17)  # => true
 puts is_odd?(-8)   # => false
 puts is_odd?(0)    # => false
 puts is_odd?(7)    # => true
+
 puts "---------------"
+
+# second solution to exercise using remainder
+# as per 'further exploration' section
+
 def is_odd_remainder?(int)
   if int.remainder(2) == 0 then false
   else true
@@ -41,3 +48,17 @@ puts is_odd_remainder?(-5)
 puts is_odd_remainder?(-4)
 puts is_odd_remainder?(0)
 puts is_odd_remainder?(5)
+
+puts "----------------"
+
+## final solution using the solution provided by course instructors
+
+def is_odd_final?(int)
+  int % 2 != 0
+end
+
+puts is_odd_final?(2)
+puts is_odd_final?(-5)
+puts is_odd_final?(-4)
+puts is_odd_final?(0)
+puts is_odd_final?(5)
