5c616f9fc6f11920f579eb02657bb33f8f362cae
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2018-12-17 08:27:39 +0200

    modified version for further_exploration and refactoring

--- exercises/2_small_problems/easy1/stringy_strings.rb
+++ exercises/2_small_problems/easy1/stringy_strings.rb
@@ -1,14 +1,13 @@
-def stringy(int)
-  counter = 0
+def stringy(int,starting_bit = 1)
   return_value = []
 
-  int.times do |number|
-    if counter.even?
-      return_value << '1'
-      counter += 1
+  int.times do |index|
+    if starting_bit == 1
+      number = index.even? ? 1 : 0
+      return_value << number
     else
-      return_value << '0'
-      counter += 1
+      number = index.even? ? 0 : 1
+      return_value << number
     end
   end
   return_value.join('')
@@ -18,3 +17,5 @@ puts stringy(6) == '101010'
 puts stringy(9) == '101010101'
 puts stringy(4) == '1010'
 puts stringy(7) == '1010101'
+puts stringy(7, 0)
+puts stringy(7, 1)
