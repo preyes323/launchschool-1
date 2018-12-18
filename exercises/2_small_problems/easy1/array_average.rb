4abf23de9519d8d1b9553f74dd08caac71ed2485
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2018-12-18 06:45:51 +0200

    solve exercise and refactor another

--- exercises/2_small_problems/easy1/array_average.rb
+++ exercises/2_small_problems/easy1/array_average.rb
@@ -1,6 +1,5 @@
 def average(array)
-  array.sum/array.size
+  array.sum/array.size.to_f
 end
 
-puts average([1, 5, 87, 45, 8, 8]) == 25
-puts average([9, 47, 23, 95, 16, 52]) == 40
+puts average([1, 5, 87, 45, 8, 8])
