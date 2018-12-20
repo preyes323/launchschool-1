72447cbfe6d3d8f879067353d23f4c6378395013
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2018-12-20 07:47:14 +0200

    added validation functionality

--- rb101_programming_foundations/lesson_2/mortgage_calculator/mortgage_calculator.rb
+++ rb101_programming_foundations/lesson_2/mortgage_calculator/mortgage_calculator.rb
@@ -7,7 +7,33 @@ def prompt(message)
   puts "=> #{message}"
 end
 
+def integer?(input)
+  input.to_i.to_s == input
+end
+
+def float?(input)
+  input.to_f.to_s == input
+end
+
+def number?(input)
+  integer?(input) || float?(input)
+end
+
+def fake_name?(input)
+  case input
+  when nil then true
+  when ' ' then true
+  when '' then true
+  else false
+  end
+end
+
+loan_input = nil
+apr_input = nil
+loan_duration_input = nil
+
 prompt("Welcome to the mortgage calculator!")
+prompt("This programme will calculate your monthly payment amount")
 prompt("What's your name?")
 
 name = nil
@@ -23,3 +49,45 @@ end
 
 prompt("Hi #{name}!")
 
+loop do # main loop
+  loop do
+    prompt("What is the loan amount in USD?")
+    loan_input = gets.chomp
+    if number?(loan_input) then break
+    else prompt("Please enter a valid number.")
+    end
+  end
+
+  loop do
+    prompt("What is your Annual Percentage Rate (APR)?")
+    prompt("Please give the number as a percentage i.e. 5")
+    apr_input = gets.chomp
+    if number?(apr_input) then break
+    else prompt("Please enter a valid number.")
+    end
+  end
+
+  loop do
+    prompt("What is the loan duration in years?")
+    loan_duration_input = gets.chomp
+    if number?(loan_duration_input) then break
+    else prompt("Please enter a valid number.")
+    end
+  end
+
+  loan_total = loan_input.to_f
+  monthly_interest_rate = apr_input.to_f / 12
+  loan_duration_months = loan_duration_input.to_i * 12
+  monthly_payment = loan_total * (monthly_interest_rate / (1 - (1 + monthly_interest_rate)**(-loan_duration_months)))
+  
+  prompt("Calculating your monthly payment...")
+  puts "----------------------------------------------"
+  prompt("Your monthly payment comes out to #{monthly_payment.round(2)} USD.")
+  puts "----------------------------------------------"
+  prompt("Would you like to do another mortgage calculation?")
+  answer = gets.chomp
+  break unless answer.downcase == ('y')
+  system('clear') || system('cls')
+end
+
+prompt("Thank you for using the mortgage calculator!")
