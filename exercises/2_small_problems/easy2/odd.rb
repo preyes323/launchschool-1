3cd43829f780f518baef109762a205a032a8f4ba
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2018-12-21 11:58:10 +0200

    solve odd and even problems

--- exercises/2_small_problems/easy2/odd.rb
+++ exercises/2_small_problems/easy2/odd.rb
@@ -2,4 +2,4 @@
 1.upto(99) { |num| puts num if num.odd? }
 
 # second solution (with identical results)
-puts (1..99).to_a.select { |num| num.odd? }
+puts (1..99).to_a.select { |num| num.odd? }1.upto(99) { |num| puts num if num.even? }
