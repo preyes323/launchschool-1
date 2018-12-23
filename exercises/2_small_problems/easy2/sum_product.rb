761895bd109918dfafe832928f31b43567ec2c20
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2018-12-23 16:45:45 +0200

    add validation for numbers and inputs

--- exercises/2_small_problems/easy2/sum_product.rb
+++ exercises/2_small_problems/easy2/sum_product.rb
@@ -12,11 +12,30 @@ def compute_product(input)
   total
 end
 
-puts ">> Please enter an integer greater than 0:"
-int = gets.chomp.to_i
+def validate_positive_integer?(input)
+  input.to_i.to_s == input && input.to_i > 0
+end
 
-puts ">> Enter 's' to compute the sum, 'p' to compute the product."
-calculation_choice = gets.chomp
+int = nil
+loop do
+  puts ">> Please enter an integer greater than 0:"
+  int = gets.chomp
+  if validate_positive_integer?(int)
+    int = int.to_i
+    break
+  else puts ">> Please enter a valid positive integer."
+  end
+end
+
+calculation_choice = nil
+loop do
+  puts ">> Enter 's' to compute the sum, 'p' to compute the product."
+  calculation_choice = gets.chomp
+  if calculation_choice == "s" || calculation_choice == "p" then break
+  else puts ">> Please enter a valid choice."
+  end
+end
+
 case calculation_choice
 when "s" then puts "The sum of the integers between 1 and #{int} is #{compute_sum(int)}."
 when "p" then puts "The product of the integers between 1 and #{int} is #{compute_product(int)}."
