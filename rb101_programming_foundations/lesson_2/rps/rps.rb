ce9041fd37859a93f1aa395a3228abb5774ddfb9
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2018-12-25 09:46:57 +0200

    refactor to include bonus choices

--- rb101_programming_foundations/lesson_2/rps/rps.rb
+++ rb101_programming_foundations/lesson_2/rps/rps.rb
@@ -1,4 +1,4 @@
-VALID_CHOICES = ['rock', 'paper', 'scissors']
+VALID_CHOICES = ['rock', 'paper', 'scissors', 'spock', 'lizard']
 
 def prompt(message)
   puts "=> #{message}"
@@ -7,7 +7,14 @@ end
 def win?(first, second)
   (first == 'rock' && second == 'scissors') ||
     (first == 'paper' && second == 'rock') ||
-    (first == 'scissors' && second == 'paper')
+    (first == 'scissors' && second == 'paper') ||
+    (first == 'rock' && second == 'lizard') ||
+    (first == 'lizard' && second == 'spock') ||
+    (first == 'spock' && second == 'scissors') ||
+    (first == 'scissors' && second == 'lizard') ||
+    (first == 'lizard' && second == 'paper') ||
+    (first == 'paper' && second == 'spock') ||
+    (first == 'spock' && second == 'rock')
 end
 
 def display_results(user_choice, computer_choice)
