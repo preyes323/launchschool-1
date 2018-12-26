43882c3c75d0e52038b2bb5303bb6dd8a5b4922d
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2018-12-26 10:18:54 +0200

    solve exercise

---
+++ rb101_programming_foundations/lesson_3/munsters.rb
@@ -0,0 +1,6 @@
+munsters_description = "The Munsters are creepy in a good way."
+
+puts munsters_description.upcase.gsub("THE MUNSTERS", "tHE mUNSTERS")
+puts munsters_description.downcase.capitalize
+puts munsters_description.downcase
+puts munsters_description.upcase
