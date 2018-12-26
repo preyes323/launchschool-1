dab17e0cea5e8bd7405712ec28cd2fc91dda3717
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2018-12-26 08:14:51 +0200

    add further exploration

--- exercises/2_small_problems/easy3/square.rb
+++ exercises/2_small_problems/easy3/square.rb
@@ -6,5 +6,14 @@ def square(num)
   multiply(num, num)
 end
 
+def power(num, powertothe)
+  total = num
+  (powertothe-1).times { total = multiply(total, num) }
+  total
+end
+
 puts square(5) == 25
 puts square(-8) == 64
+
+puts power(5, 2)
+puts power(3, 3)
