47a1202d0381137c0fcc9bf372ddad70d06096eb
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2018-12-26 08:49:57 +0200

    solved a second way

--- exercises/2_small_problems/easy3/odd_lists.rb
+++ exercises/2_small_problems/easy3/odd_lists.rb
@@ -1,3 +1,11 @@
+def oddities2(array)
+  array.select {|item| array.index(item).even? }
+end
+
+def evenities2(array)
+  array.select {|item| array.index(item).odd? }
+end
+
 def oddities(array)
   count = 0
   final_array = []
@@ -8,8 +16,22 @@ def oddities(array)
   final_array
 end
 
+def evenities(array)
+  count = 0
+  final_array = []
+  array.each do |item|
+    final_array << item if count.odd?
+    count +=1
+  end
+  final_array
+end
+
 puts oddities([2, 3, 4, 5, 6]) == [2, 4, 6]
 puts oddities([1, 2, 3, 4, 5, 6]) == [1, 3, 5]
 puts oddities(['abc', 'def']) == ['abc']
 puts oddities([123]) == [123]
 puts oddities([]) == []
+
+puts evenities([1,2,3,4,5]) == [2, 4]
+
+puts oddities2([2,3,4,5,6]) == [2,4,6]
