3befeaef08efb8a713bbd199c6ab8d88210594e5
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2018-12-27 08:37:14 +0200

    fix problem

---
+++ rb101_programming_foundations/lesson_3/factors.rb
@@ -0,0 +1,14 @@
+def factors(number)
+  divisor = number
+  factors = []
+  while divisor > 0
+    factors << number / divisor if number % divisor == 0
+    divisor -= 1
+  end
+  factors
+end
+
+puts factors(-1)
+puts factors(0)
+puts factors(2)
+puts factors(3)
