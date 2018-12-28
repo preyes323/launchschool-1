65a4a353131900cdbf8e7be486faf2b1e284fe7a
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2018-12-28 17:40:33 +0200

    start the exercise

---
+++ exercises/2_small_problems/easy4/short_long_short.rb
@@ -0,0 +1,7 @@
+def shortlongshort(string1, string2)
+  string1.size <=> string2.size
+end
+
+puts short_long_short('abc', 'defgh') == "abcdefghabc"
+puts short_long_short('abcde', 'fgh') == "fghabcdefgh"
+puts short_long_short('', 'xyz') == "xyz"
