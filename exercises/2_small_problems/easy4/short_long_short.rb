9a20396a363c3c63d92bec483fcd53faa142eb28
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2018-12-28 19:13:19 +0200

    solve problem

--- exercises/2_small_problems/easy4/short_long_short.rb
+++ exercises/2_small_problems/easy4/short_long_short.rb
@@ -1,5 +1,9 @@
-def shortlongshort(string1, string2)
-  string1.size <=> string2.size
+def short_long_short(string1, string2)
+  if (string1.size <=> string2.size) == 1
+    string2 + string1 + string2
+  else
+    string1 + string2 + string1
+  end
 end
 
 puts short_long_short('abc', 'defgh') == "abcdefghabc"
