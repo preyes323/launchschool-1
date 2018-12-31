d3954dc54238b368f03b346c4ba91e0ee3438a69
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2018-12-31 16:53:28 +0200

    delete whitespace

--- exercises/2_small_problems/easy4/leap_years1.rb
+++ exercises/2_small_problems/easy4/leap_years1.rb
@@ -14,7 +14,7 @@
 def leap_year?(year)
   if year % 4 == 0 && year % 100 == 0 && year % 400 == 0
     true
-  elsif year % 4 == 0 && year % 100 == 0 
+  elsif year % 4 == 0 && year % 100 == 0
     false
   elsif year % 4 == 0
     true
