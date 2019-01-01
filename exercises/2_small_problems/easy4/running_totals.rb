58d1cd3502d3bd0fef8a927fa80ded9cd157a7f0
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2019-01-01 09:26:55 +0200

    solve with each_with_object

--- exercises/2_small_problems/easy4/running_totals.rb
+++ exercises/2_small_problems/easy4/running_totals.rb
@@ -14,7 +14,19 @@ puts running_total([3]) == [3]
 puts running_total([]) == []
 
 # a different way to solve this problem with map
+
 def running_total2(array)
   sum = 0
   array.map { |number| sum += number }
+end
+
+# solving the problem with each_with_object
+
+def running_total3(array)
+  total = 0
+  new_array = array.each_with_object([]) do |number, new_array|
+    new_array << total + number
+    total += number
+  end
+  puts new_array
 end
