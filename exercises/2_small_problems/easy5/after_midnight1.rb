cd809e883f88084961b97ab8ca62f6d21dcd89c1
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2019-01-03 07:36:13 +0200

    add files

---
+++ exercises/2_small_problems/easy5/after_midnight1.rb
@@ -0,0 +1,11 @@
+def time_of_day(minutes)
+  
+end
+
+time_of_day(0) == "00:00"
+time_of_day(-3) == "23:57"
+time_of_day(35) == "00:35"
+time_of_day(-1437) == "00:03"
+time_of_day(3000) == "02:00"
+time_of_day(800) == "13:20"
+time_of_day(-4231) == "01:29"
