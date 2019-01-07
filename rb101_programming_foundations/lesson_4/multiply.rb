cc81023343d47ce12a31521aca8ab5d67df0fa4a
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2019-01-07 07:23:57 +0200

    solve problem

---
+++ rb101_programming_foundations/lesson_4/multiply.rb
@@ -0,0 +1,16 @@
+def multiply(numbers_array, multiplier)
+  counter = 0
+  multiplied_numbers = []
+
+  loop do
+    break if counter == numbers_array.size
+
+    multiplied_numbers << numbers_array[counter] * multiplier
+    
+    counter += 1
+  end
+  multiplied_numbers
+end
+
+my_numbers = [1, 4, 3, 7, 2, 6]
+puts multiply(my_numbers, 3) # => [3, 12, 9, 21, 6, 18]
