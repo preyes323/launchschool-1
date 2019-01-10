174c0155439503ab4fe9b5becc42d2abe3463063
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2019-01-10 08:54:09 +0200

    solve problem

---
+++ rb101_programming_foundations/lesson_4/p2.rb
@@ -0,0 +1,8 @@
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10, "Marilyn" => 22, "Spot" => 237 }
+
+# Add up all of the ages from the Munster family hash.
+
+# version 1
+
+total_ages = ages.values.sum
+puts total_ages
