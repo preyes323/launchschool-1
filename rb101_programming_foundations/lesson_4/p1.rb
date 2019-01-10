f78b736130b150c6c0d49e3c0ecb5bef1dadf383
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2019-01-10 08:47:54 +0200

    solve problem in two different ways

---
+++ rb101_programming_foundations/lesson_4/p1.rb
@@ -0,0 +1,25 @@
+flintstones = ["Fred", "Barney", "Wilma", "Betty", "Pebbles", "BamBam"]
+
+# Turn this array into a hash where the names are the keys
+# and the values are the positions in the array.
+
+# version 1
+
+hash1 = Hash.new
+
+flintstones.each_with_index do |name, index|
+  hash1[name] = index
+end
+
+p hash1
+
+# version 2
+
+hash2 = flintstones.each_with_object({}) do |name, hash|
+  hash[name] = flintstones.index(name)
+end
+
+p hash2
+
+# version 3
+
