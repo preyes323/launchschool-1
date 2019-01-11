490e45f039c6d2e9ad0cf960836a1d832969b7d7
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2019-01-11 09:39:18 +0200

    solve problem

---
+++ rb101_programming_foundations/lesson_4/p10.rb
@@ -0,0 +1,24 @@
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+# Modify the hash such that each member of the Munster family
+# has an additional "age_group" key that has one of three values
+# describing the age group the family member is in (kid, adult, or senior).
+
+names = munsters.keys
+names.each do |name|
+  if munsters[name]['age'] <= 17
+    munsters[name]['age_group'] = 'kid'
+  elsif munsters[name]['age'] <= 64
+    munsters[name]['age_group'] = 'adult'
+  else
+    munsters[name]['age_group'] = 'senior'
+  end
+end
+
+p munsters
