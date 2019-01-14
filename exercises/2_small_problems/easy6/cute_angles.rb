0b48985a53073a97251ee82f8fd3d6c7cd10d1c8
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2019-01-14 07:38:20 +0200

    solve problem(s)

---
+++ exercises/2_small_problems/easy6/cute_angles.rb
@@ -0,0 +1,19 @@
+# coding: utf-8
+DEGREE = "\xC2\xB0"
+MINUTES_PER_DEGREE = 60
+SECONDS_PER_MINUTE = 60
+
+def dms(float)
+  wholedegree = float.divmod(1)[0]
+  remainder = (float - wholedegree).round(10)
+  minutes = (remainder * MINUTES_PER_DEGREE).divmod(1)[0]
+  seconds = ((remainder * MINUTES_PER_DEGREE).divmod(1)[1] * SECONDS_PER_MINUTE).round(0)
+  %Q(#{wholedegree}#{DEGREE}#{format('%02d', minutes)}'#{format('%02d', seconds)}")
+end
+
+puts dms(30) == %(30°00'00")
+puts dms(76.73) == %(76°43'48")
+puts dms(254.6) == %(254°36'00")
+puts dms(93.034773) == %(93°02'05")
+puts dms(0) == %(0°00'00")
+puts dms(360) == %(360°00'00") || dms(360) == %(0°00'00")
