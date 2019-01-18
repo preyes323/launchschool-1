53cec8f7a7d68bbb4bcf4b5090f2ba96b9979a59
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2019-01-18 08:10:27 +0200

    solve further exploration

--- exercises/2_small_problems/easy6/reversed_arrays2.rb
+++ exercises/2_small_problems/easy6/reversed_arrays2.rb
@@ -14,3 +14,14 @@ new_list = reverse(list)              # => [2, 3, 1]
 puts list.object_id != new_list.object_id  # => true
 puts list == [1, 3, 2]                     # => true
 puts new_list == [2, 3, 1]                 # => true
+
+def reverse2(array)
+  array.each_with_object([]) do |item, new_array|
+    new_array.unshift(item)
+  end
+end
+
+puts reverse2([1,2,3,4]) == [4,3,2,1]          # => true
+puts reverse2(%w(a b e d c)) == %w(c d e b a)  # => true
+puts reverse2(['abc']) == ['abc']              # => true
+puts reverse2([]) == []                        # => true
