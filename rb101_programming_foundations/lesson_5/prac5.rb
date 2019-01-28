344097aa6e6a3aa9974b14fa4cfbdef680415785
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2019-01-28 08:50:56 +0000

    solve problem

---
+++ rb101_programming_foundations/lesson_5/prac5.rb
@@ -0,0 +1,13 @@
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+a = munsters.select do |_, value|
+  value["gender"] == "male"
+end
+
+p a
