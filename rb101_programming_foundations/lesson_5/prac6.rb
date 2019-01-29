2dcd8f4a0b85c814dc573fba0dd53e11347d7e95
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2019-01-29 09:18:50 +0000

    solve problem

---
+++ rb101_programming_foundations/lesson_5/prac6.rb
@@ -0,0 +1,13 @@
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+munsters.each do |key, value|
+  age = value["age"]
+  gender = value["gender"]
+  puts "#{key} is a #{age}-year-old #{gender}."
+end
