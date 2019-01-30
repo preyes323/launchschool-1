53c89b36740a2a0db0b4617923f317c0c8e1b498
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2019-01-30 09:07:13 +0000

    solve properly

--- rb101_programming_foundations/lesson_5/prac11.rb
+++ rb101_programming_foundations/lesson_5/prac11.rb
@@ -1,8 +1,6 @@
 arr = [[2], [3, 5, 7], [9], [11, 13, 15]]
 
-new_array = []
-arr.each do |item|
-  new_array << item.select { |num| num % 3 == 0 }
+arr.each_with_object([]) do |item, array|
+  array << item.select { |num| num % 3 == 0 }
 end
 
-p new_array
