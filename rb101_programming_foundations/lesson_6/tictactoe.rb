5ef89d56ed0f6fa91ae4174d80c4b13423c656dc
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2019-02-03 12:30:26 +0000

    complete the main game loop

--- rb101_programming_foundations/lesson_6/tictactoe.rb
+++ rb101_programming_foundations/lesson_6/tictactoe.rb
@@ -1,5 +1,3 @@
-require "pry"
-
 INITIAL_MARKER = " "
 PLAYER_MARKER = "X"
 COMPUTER_MARKER = "O"
@@ -9,6 +7,7 @@ def prompt(msg)
 end
 
 def display_board(brd)
+  system("cls") || system("clear")
   puts ""
   puts "     |     |"
   puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
@@ -31,7 +30,6 @@ def initialize_board
 end
 
 def empty_squares(brd)
-  binding.pry
   brd.keys.select { |num| brd[num] == INITIAL_MARKER }
 end
 
@@ -46,9 +44,28 @@ def player_places_piece!(brd)
   brd[square] = PLAYER_MARKER
 end
 
+def computer_places_piece!(brd)
+  square = empty_squares(brd).sample
+  brd[square] = COMPUTER_MARKER
+end
+
+def board_full?(brd)
+  empty_squares(brd).empty?
+end
+
+def someone_won?(brd)
+  false
+end
+
 board = initialize_board
 display_board(board)
 
-player_places_piece!(board)
-puts board.inspect
+loop do
+  player_places_piece!(board)
+  computer_places_piece!(board)
+  display_board(board)
+  break if someone_won?(board) || board_full?(board)
+end
+
 display_board(board)
+
