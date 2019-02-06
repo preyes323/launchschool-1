167ae1a97f21756150120632736b67e9d1831b9c
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2019-02-06 20:58:41 +0000

    solve problem

---
+++ exercises/2_small_problems/easy9/uppercase.rb
@@ -0,0 +1,10 @@
+def uppercase?(string)
+  string.chars.all? {|char| char == char.upcase }
+end
+
+puts uppercase?('t') == false
+puts uppercase?('T') == true
+puts uppercase?('Four Score') == false
+puts uppercase?('FOUR SCORE') == true
+puts uppercase?('4SCORE!') == true
+puts uppercase?('') == true
