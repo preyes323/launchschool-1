7676200815b1a9b604a711f6b102e3b17a6a2c18
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2019-02-10 12:14:25 +0000

    setup initial gameplay with cards dealt and displaying hands

--- rb101_programming_foundations/lesson_6/twenty_one.rb
+++ rb101_programming_foundations/lesson_6/twenty_one.rb
@@ -1,5 +1,21 @@
+require 'pry'
+
 CARDS = %w(A 2 3 4 5 6 7 8 9 10 J Q K)
 SUITS = %w(H C D S)
+CARDS_NAMES = { "A" => "Ace",
+                "2" => "2",
+                "3" => "3",
+                "4" => "4",
+                "5" => "5",
+                "6" => "6",
+                "7" => "7",
+                "8" => "8",
+                "9" => "9",
+                "10" => "10",
+                "J" => "Jack",
+                "Q" => "Queen",
+                "K" => "King"
+              }
 
 def prompt(string)
   puts "=> #{string}"
@@ -26,6 +42,24 @@ def initial_deal(deck, player_deck, dealer_deck)
   deal_cards(deck, dealer_deck, 2)
 end
 
+def list_cards(array)
+  cards = []
+  array.each do |sub_array|
+    cards << CARDS_NAMES[sub_array.first]
+  end
+  joinor(cards)
+end
+
+def joinor(array, divider=",", word="and")
+  string = ""
+  initial_numbers = array.slice(0..-2)
+  last_number = array.last
+  initial_numbers.each do |num|
+    string << "#{num}#{divider} "
+  end
+  string << "#{word} #{last_number}"
+end
+
 # gameplay starts here
 
 loop do
@@ -33,11 +67,13 @@ loop do
   dealer_cards = []
   game_deck = initialize_deck
   initial_deal(game_deck, player_cards, dealer_cards)
-  p game_deck.size
-  p player_cards
-  p dealer_cards
+
+  system('cls') || system('clear')
+  prompt "Player: #{list_cards(player_cards)}"
+  prompt "Dealer: #{list_cards(dealer_cards)}"
+
   prompt "Do you want to play again? (y or n)"
-  answer = gets.chomp
+ answer = gets.chomp
   break if answer == "n"
 end
 
