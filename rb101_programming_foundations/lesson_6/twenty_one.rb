9aa8dc703a1e9ac8cf15db076520abcb8e0388ba
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2019-02-10 16:31:09 +0000

    complete a glitchy version of full gameplay

--- rb101_programming_foundations/lesson_6/twenty_one.rb
+++ rb101_programming_foundations/lesson_6/twenty_one.rb
@@ -70,6 +70,43 @@ def joinor(arr, delimiter=', ', word='and')
   end
 end
 
+def total(cards)
+  values = cards.map { |card| card[0] }
+
+  sum = 0
+  values.each do |value|
+    if value == "A"
+      sum += 11
+    elsif value.to_i == 0 # J, Q, K
+      sum += 10
+    else
+      sum += value.to_i
+    end
+  end
+
+  # correct for Aces
+  values.select { |value| value == "A" }.count.times do
+    sum -= 10 if sum > 21
+  end
+
+  sum
+end
+
+def detect_winner(cards)
+  total(cards) == 21
+end
+
+def busted?(cards)
+  total(cards) > 21
+end
+
+def alternate_player(current_player)
+  case current_player
+  when "c" then "p"
+  when "p" then "c"
+  end
+end
+
 # gameplay starts here
 
 loop do
@@ -77,14 +114,54 @@ loop do
   dealer_cards = []
   game_deck = initialize_deck
   initial_deal(game_deck, player_cards, dealer_cards)
-
+  current_player = ""
   system('cls') || system('clear')
-  prompt "Player: #{show_cards(player_cards, "player")}"
-  prompt "Dealer: #{show_cards(dealer_cards, "dealer")}"
+  loop do
+    prompt "Which user to start? (player(p) or computer(c) or random(r))?"
+    current_player = gets.chomp
+    current_player = ["p", "c"].sample if current_player.downcase == "r"
+    break if ["p", "c"].include?(current_player.downcase)
+  end
+  
+  system('cls') || system('clear')
+  answer = nil
+  loop do
+    prompt "Player: #{show_cards(player_cards, "player")}"
+    puts "Player total: #{total(player_cards)}"
+    prompt "Dealer: #{show_cards(dealer_cards, "dealer")}"
+    puts "Dealer total: #{total(dealer_cards)}"
+    prompt "hit or stay?"
+    answer = gets.chomp
+    deal_cards(game_deck, player_cards, 1) if answer == "hit"
+    break if answer == 'stay' || busted?(player_cards)
+  end
 
-  prompt "Do you want to play again? (y or n)"
-  answer = gets.chomp
-  break if answer == "n"
+  if busted?(player_cards)
+    prompt "Player busted out! Dealer wins!"
+    prompt "Do you want to play again? (y or n)"
+    end_answer = gets.chomp
+    break if end_answer == "n"
+  else
+    prompt "You chose to stay!"
+  end
+
+  loop do
+    prompt "Player: #{show_cards(player_cards, "player")}"
+    puts "Player total: #{total(player_cards)}"
+    prompt "Dealer: #{show_cards(dealer_cards, "dealer")}"
+    puts "Dealer total: #{total(dealer_cards)}"
+    deal_cards(game_deck, dealer_cards, 1) if total(dealer_cards) <= 17
+    break if busted?(dealer_cards)
+  end
+
+  if busted?(dealer_cards)
+    prompt "Dealer busted out! Player wins!"
+    prompt "Do you want to play again? (y or n)"
+    end_answer = gets.chomp
+    break if end_answer == "n"
+  else
+    prompt "Dealer chose to stay!"
+  end
 end
 
 prompt "Thanks for playing Twenty-One!"
