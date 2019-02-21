e1547198022b2610260094efac684876a07d8707
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2019-02-21 09:36:16 +0000

    comment out the large numbers

--- exercises/2_small_problems/medium1/fibonacci.rb
+++ exercises/2_small_problems/medium1/fibonacci.rb
@@ -10,7 +10,7 @@ puts fibonacci(4) == 3
 puts fibonacci(5) == 5
 puts fibonacci(12) == 144
 puts fibonacci(20) == 6765
-puts fibonacci(50)
-puts fibonacci(20) == 6765
-puts fibonacci(100) == 354224848179261915075
-puts fibonacci(100_001) # => 4202692702.....8285979669707537501
+# puts fibonacci(50)
+# puts fibonacci(20) == 6765
+# puts fibonacci(100) == 354224848179261915075
+# puts fibonacci(100_001) # => 4202692702.....8285979669707537501
