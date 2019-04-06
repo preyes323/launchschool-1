6226b98cacd2dc506e37b84769d45c4acd96a793
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2019-04-06 16:06:03 +0100

    delete extra comments

--- exercises/2_small_problems/medium2/neutralize.rb
+++ exercises/2_small_problems/medium2/neutralize.rb
@@ -10,5 +10,3 @@ def neutralize(sentence)
 end
 
 puts neutralize('These dull boring cards are part of a chaotic board game.')
-# Expected: These cards are part of a board game.
-# Actual: These boring cards are part of a board game.
