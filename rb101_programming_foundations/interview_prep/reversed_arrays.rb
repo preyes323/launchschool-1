8fc7eee67ce0cc64bc1418186ae5ef6acfb34d4b
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2019-04-08 16:59:56 +0100

    solve problem

---
+++ rb101_programming_foundations/interview_prep/reversed_arrays.rb
@@ -0,0 +1,16 @@
+require 'pry'
+
+def reverse!(arr)
+  
+end
+
+=begin
+
+
+=end
+
+list = [1,2,3,4]
+result = reverse!(list)
+puts result == [4, 3, 2, 1]
+puts list == [4, 3, 2, 1]
+puts list.object_id == result.object_id
