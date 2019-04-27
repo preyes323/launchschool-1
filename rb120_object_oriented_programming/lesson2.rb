b6621c3a9e9e244bcd866ced137858d31538aa5e
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2019-04-27 15:09:42 +0100

    refactor exercise two

--- rb120_object_oriented_programming/lesson2.rb
+++ rb120_object_oriented_programming/lesson2.rb
@@ -1,12 +1,20 @@
 class Person
-  attr_accessor :name
+  attr_accessor :first_name, :last_name
 
-  def initialize(name)
-    @name = name
+  def initialize(full_name)
+    parts = full_name.split(" ")
+    @first_name = parts.first
+    @last_name = parts.size > 1 ? parts.last : ''
+  end
+
+  def name
+    "#{@first_name} #{@last_name}"
   end
 end
 
-bob = Person.new('bob')
-puts bob.name                  # => 'bob'
-bob.name = 'Robert'
+bob = Person.new('Robert')
 puts bob.name                  # => 'Robert'
+puts bob.first_name            # => 'Robert'
+puts bob.last_name             # => ''
+bob.last_name = 'Smith'
+puts bob.name                  # => 'Robert Smith'
