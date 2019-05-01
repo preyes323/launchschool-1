06539f416c5e3e1f86716f32a534a3aa1eecb228
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2019-05-01 11:57:11 +0100

    add lizard and spock to the gameplay

--- rb120_object_oriented_programming/lesson_2/rps/oorps.rb
+++ rb120_object_oriented_programming/lesson_2/rps/oorps.rb
@@ -1,11 +1,19 @@
 class Move
-  VALUES = ["rock", "paper", "scissors"]
+  VALUES = ["rock", "paper", "scissors", "spock", "lizard"]
   def initialize(value)
     @value = value
   end
 
   def scissors?
     @value == 'scissors'
+  end
+
+  def spock?
+    @value == "spock"
+  end
+
+  def lizard?
+    @value == "lizard"
   end
 
   def rock?
@@ -19,13 +27,27 @@ class Move
   def >(other_move)
     (rock? && other_move.scissors?) ||
       (paper? && other_move.rock?) ||
-      (scissors? && other_move.paper?)
+      (scissors? && other_move.paper?) ||
+      (rock? && other_move.lizard?) ||
+      (paper? && other_move.spock?) ||
+      (scissors? && other_move.lizard?) ||
+      (lizard? && other_move.spock?) ||
+      (lizard? && other_move.paper?) ||
+      (spock? && other_move.scissors?) ||
+      (spock? && other_move.rock?)
   end
 
   def <(other_move)
     (rock? && other_move.paper?) ||
       (paper? && other_move.scissors?) ||
-      (scissors? && other_move.rock?)
+      (scissors? && other_move.rock?) ||
+      (rock? && other_move.spock?) ||
+      (paper? && other_move.lizard?) ||
+      (scissors? && other_move.spock?) ||
+      (lizard? && other_move.rock?) ||
+      (lizard? && other_move.scissors?) ||
+      (spock? && other_move.paper?) ||
+      (spock? && other_move.lizard?)
   end
 
   def to_s
@@ -56,7 +78,7 @@ class Human < Player
   def choose
     choice = nil
     loop do
-      puts "Please choose rock, paper, or scissors:"
+      puts "Please choose rock, paper, scissors, spock or lizard:"
       choice = gets.chomp
       break if Move::VALUES.include?(choice)
       puts "Sorry, invalid choice."
@@ -85,11 +107,11 @@ class RPSGame
   end
 
   def display_welcome_message
-    puts "Welcome to Rock, Paper, Scissors!"
+    puts "Welcome to Rock, Paper, Scissors, Lizard, Spock!"
   end
 
   def display_goodbye_message
-    puts "Thanks for playing Rock, Paper, Scissors. Good bye!"
+    puts "Thanks for playing Rock, Paper, Scissors, Lizard, Spock. Good bye!"
   end
 
   def display_moves
